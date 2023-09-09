bbd18643d2fb77cfc85b22c1ec7ede7f5ea33b54
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-09-09 17:52:32 -0500

    feat: add new route to index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 const express = require("express");
 const router = express.Router();
 
+// Sample messages
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "Express", messages });
+});
+
+// Get on new
+router.get("/new", function (req, res, next) {
+  res.render("form", { title: "New" });
+});
+
+router.post("/new", function (req, res, next) {
+  const { message, authorName } = req.body;
+
+  messages.push({
+    text: message,
+    user: authorName,
+    added: new Date(),
+  });
+
+  res.redirect("/");
 });
 
 module.exports = router;
