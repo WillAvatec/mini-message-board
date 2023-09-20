8aef7b0e635367362828af72579a6a3f3a8bff65
Emanuel <115133403+WillAvatec@users.noreply.github.com>
2023-09-20 15:40:45 -0500

    feat: Move controller logic into its own file

---
+++ controllers/main.js
@@ -0,0 +1,27 @@
+const Message = require("../models/message");
+const asyncHandler = require("express-async-handler");
+
+// Display all messages on 'index'
+exports.index = asyncHandler(async (req, res, next) => {
+  const allMessages = await Message.find().sort({ date: 1 }).exec();
+  res.render("index", { messages: allMessages });
+});
+
+// Display form to create new messages on GET
+exports.form_get = asyncHandler(async (req, res, next) => {
+  res.render("form");
+});
+
+// Handle form body data on POST
+exports.form_post = asyncHandler(async (req, res, next) => {
+  const { author, content } = req.body;
+
+  const newMessage = new Message({
+    author,
+    content,
+    date: new Date(),
+  });
+
+  await newMessage.save();
+  res.redirect("/");
+});
